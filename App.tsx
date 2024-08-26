9a9ab9d5089928c11ded98a49c013f12f827b3fa
Yuto Urashima <yuto.urashima24@gmail.com>
2024-08-26 23:11:02 +0900

    feat: 絵文字を動かせるようにした

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
 import Button from '@/components/button'
 import CircleButton from '@/components/circleButton'
 import EmojiList from '@/components/emojiList'
@@ -50,7 +52,7 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
         {pickedEmoji && <EmojiSticker imageSize={40} stickerSource={pickedEmoji} />}
@@ -73,7 +75,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   )
 }
 
